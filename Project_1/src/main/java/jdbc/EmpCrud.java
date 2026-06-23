17d571772269cab7d3553b86c957dd3c9a9eae80
Swaraj-Bhagwat <swarajbhagwat21@gmail.com>
2026-06-23 16:02:24 +0530

    Updated EmpCrud.java

--- Project_1/src/main/java/jdbc/EmpCrud.java
+++ Project_1/src/main/java/jdbc/EmpCrud.java
@@ -30,21 +30,21 @@ public class EmpCrud {
 			
 			
 			// Create Statement
-			//String insertQuery = "INSERT INTO employee VALUES(101,'Ram', 98000.0,'Developer')";
-			//String insertQuery1 = "INSERT INTO employee VALUES(102,'Krushna', 42000.0,'HR')";
-			//String insertQuery2 = "INSERT INTO employee VALUES(103,'Govind', 22000.0,'HR')";
-			//String insertQuery3 = "INSERT INTO employee VALUES(104,'Rakesh', 90000.0,'Developer')";
-			//String insertQuery4 = "INSERT INTO employee VALUES(105,'Radha', 74000.0,'Marketing')";
+			String insertQuery = "INSERT INTO employee VALUES(101,'Ram', 98000.0,'Developer')";
+			String insertQuery1 = "INSERT INTO employee VALUES(102,'Krushna', 42000.0,'HR')";
+			String insertQuery2 = "INSERT INTO employee VALUES(103,'Govind', 22000.0,'HR')";
+			String insertQuery3 = "INSERT INTO employee VALUES(104,'Rakesh', 90000.0,'Developer')";
+			String insertQuery4 = "INSERT INTO employee VALUES(105,'Radha', 74000.0,'Marketing')";
 
 			Statement st = conn.createStatement();
 			
 			
 			//Execute Query 
-			//st.execute(insertQuery);
-			//st.execute(insertQuery1);
-			//st.execute(insertQuery2);
-			//st.execute(insertQuery3);
-			//st.execute(insertQuery4);
+			st.execute(insertQuery);
+			st.execute(insertQuery1);
+			st.execute(insertQuery2);
+			st.execute(insertQuery3);
+			st.execute(insertQuery4);
 			
 			
 			//close connection
